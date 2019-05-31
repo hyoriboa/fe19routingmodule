1fcc981f364c317ff32a9afd67c34bf5bcee066c
hyoriboa <hongocluan1993@gmail.com>
2019-05-31 19:19:47 +0700

    'up'

--- src/app/home/form/form-routing.module.ts
+++ src/app/home/form/form-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DangKyComponent } from './dang-ky/dang-ky.component';
+import { FormComponent } from './form.component';
+import { DangNhapComponent } from './dang-nhap/dang-nhap.component';
 
 const routes: Routes = [
+
+    {
+        path: "",
+        component: FormComponent,
+        children: [
+            //Dang ki
+            {
+                path:"",
+                component: DangKyComponent
+            },
+
+            {
+                path:"dang-ky",
+                component: DangKyComponent
+            },
+
+            //Dang nhap
+            {
+                path:"dang-nhap",
+                component: DangNhapComponent
+            },
+
+        ]
+    },
+
 
         
 ];
