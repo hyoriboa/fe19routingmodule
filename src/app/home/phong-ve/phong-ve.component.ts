7bf0a6d674ef18107662d6bbc35c7a591c0ed1c4
hyoriboa <hongocluan1993@gmail.com>
2019-05-31 19:02:18 +0700

    'update'

---
+++ src/app/home/phong-ve/phong-ve.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {QuanLiPhimService} from './../../../_core/services/quan-li-phim.service';
+
+@Component({
+  selector: 'app-phong-ve',
+  templateUrl: './phong-ve.component.html',
+  styleUrls: ['./phong-ve.component.scss']
+})
+export class PhongVeComponent implements OnInit {
+
+  public maLichChieu:any;
+  public danhSachGhe:any;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private quanLiPhimService: QuanLiPhimService
+    ) { }
+
+  ngOnInit() {
+    this.getParams();
+    this.getChiTietPhongVe();
+  }
+
+  getParams(){
+    this.maLichChieu = this.activatedRoute.snapshot.paramMap.get("maLichChieu");
+  }
+
+  getChiTietPhongVe(){
+    this.quanLiPhimService.chiTietPhongVe(this.maLichChieu).subscribe((data)=>{
+      console.log(data);
+      this.danhSachGhe = data.DanhSachGhe;
+    });
+  }
+
+  chonGhe(objGhe){
+    console.log(objGhe);
+    
+  }
+
+}
